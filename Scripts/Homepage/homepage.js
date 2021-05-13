48fd123c7d311cd18d49a58c5df805d07c2c851a
Muhammad Zulkifel <zulkifel@gmail.com>
2021-05-13 14:58:33 +0500

    Carousel Shit Done

--- Scripts/Homepage/homepage.js
+++ Scripts/Homepage/homepage.js
@@ -2,3 +2,29 @@
     obj.style.height = obj.contentWindow.document.documentElement.scrollHeight + 'px';
     obj.style.width = obj.contentWindow.document.documentElement.scrollHeight + 'px';
 }
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    autoplay:true,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:false,
+            lazyLoad: true
+        },
+        600:{
+            items:2,
+            nav:false,
+            loop:true,
+            lazyLoad: true
+        },
+        1000:{
+            items:3,
+            nav:false,
+            loop:true,
+            lazyLoad: false
+        }
+    }
+});
